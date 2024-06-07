86c97447de4a88fc911c987902880944daaa84c6
Cookers <simply.proctor@gmail.com>
2024-06-07 23:12:19 +0200

    update models regex, corrected splitting errors

--- recipe_helper_website/app/models.py
+++ recipe_helper_website/app/models.py
@@ -2,26 +2,26 @@ import re
 
 from Id_assigner import ID
 # find all units
-unit_pattern = r"(cup|g|oz|ml|kg|lb|tsp|tbsp|pound|cups|fl oz)"
+unit_pattern = r"(cup|oz|ml|kg|tsp|tbsp|cups|fl oz)"
 #
-unit_pattern1 = re.compile(r"(\d+\s?\w+)\/\d+\w+", re.MULTILINE)
+pattern1 = re.compile(r"(\d+\s?\w+)\/\d+\w+", re.MULTILINE)
 # brackets  (3 1/2 cups)
-unit_pattern2 = re.compile(
-    r"\s\(\d+\s?\w+(\/\d+\s?\w+)?\)", re.MULTILINE,)
+pattern2 = re.compile(
+    r"\s\(\d+.?\s?\w+(\/\d+\s?\w+)?\)", re.MULTILINE,)
 # 1 /3
-unit_pattern3 = re.compile(r"(\d+)\s?(\/\d)")
+pattern3 = re.compile(r"(\d+)\s?(\/\d)")
 # tablespoon, tbsp., Tbsp., Tbsp
-unit_pattern4 = re.compile(r"tablespoons?|tbsp\.", re.IGNORECASE)
+pattern4 = re.compile(r"tablespoons?|tbsp\.", re.IGNORECASE)
 # teaspoon, tsp., Tsp., Tsp
-unit_pattern5 = re.compile(r"teaspoons?|tsp\.", re.IGNORECASE)
+pattern5 = re.compile(r"teaspoons?|tsp\.", re.IGNORECASE)
 # 1⁄2
-unit_pattern6 = re.compile(r"(\d+)\u2044(\d+)")
+patter6 = re.compile(r"(\d+)\u2044(\d+)")
 # grams, Grams
-unit_pattern7 = re.compile(r"grams?|Grams?", re.IGNORECASE)
+pattern7 = re.compile(r"grams?|Grams?", re.IGNORECASE)
 # millilitres, ML
-unit_pattern8 = re.compile(r"millilitres?|ML?", re.IGNORECASE)
+pattern8 = re.compile(r"millilitres?|ML", re.IGNORECASE)
 # ounces, OZ
-unit_pattern9 = re.compile(r"ounces?|OZ?", re.IGNORECASE)
+pattern9 = re.compile(r"ounces?|OZ", re.IGNORECASE)
 
 
 class Recipe:
@@ -54,17 +54,18 @@ class Recipe:
         substitution7 = "g"
         substitution8 = "ml"
         substitution9 = "oz"
-        standardised = re.sub(unit_pattern6, substitution6, ingredient, 0)
+        #
+        standardised = re.sub(patter6, substitution6, ingredient, 0)
         standardised = re.sub(
-            unit_pattern1, substitution, standardised, 0)
+            pattern1, substitution, standardised, 0)
         standardised = re.sub(
-            unit_pattern2, substitution2, standardised, 0)
-        standardised = re.sub(unit_pattern3, substitution3, standardised, 0)
-        standardised = re.sub(unit_pattern4, substitution4, standardised, 0)
-        standardised = re.sub(unit_pattern5, substitution5, standardised, 0)
-        standardised = re.sub(unit_pattern7, substitution7, standardised, 0)
-        standardised = re.sub(unit_pattern8, substitution8, standardised, 0)
-        standardised = re.sub(unit_pattern9, substitution9, standardised, 0)
+            pattern2, substitution2, standardised, 0)
+        standardised = re.sub(pattern3, substitution3, standardised, 0)
+        standardised = re.sub(pattern4, substitution4, standardised, 0)
+        standardised = re.sub(pattern5, substitution5, standardised, 0)
+        standardised = re.sub(pattern7, substitution7, standardised, 0)
+        standardised = re.sub(pattern8, substitution8, standardised, 0)
+        standardised = re.sub(pattern9, substitution9, standardised, 0)
 
         return Ingredient(standardised)
 
@@ -74,7 +75,7 @@ class Ingredient:
     def __init__(self, ingredient):
         if bool(re.search(unit_pattern, ingredient)):
             self.convertable = True
-            split = re.split(unit_pattern, ingredient, maxsplit=2, flags=re.I)
+            split = re.split(unit_pattern, ingredient, maxsplit=1, flags=re.I)
             self.quantity = split[0]
             self.unit = split[1]
             self.item = split[2]
