15f799b1b4623f97748e0d6df0ec8c269a53b34b
Cookers <simply.proctor@gmail.com>
2024-06-08 12:08:26 +0200

    reame recipe constructor file, delete website.py

--- recipe_helper_website/app/recipe constructor.py
+++
@@ -1,9 +0,0 @@
-from classes.Recipe_class import Recipe
-
-
-def recipe_constructor(data):
-    recipes = []
-    for recipe in data:
-        recipes.append(
-            Recipe(recipe['Recipe'], recipe['URL'], recipe['Image'], recipe['Ingredients']))
-    return recipes
